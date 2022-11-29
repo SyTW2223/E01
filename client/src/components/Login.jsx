45d11e672ca0706fff12b5cca4a14d7a74ddb795
juanmdom <juanmdom@gmail.com>
2022-11-29 10:21:40 +0000

    estilos aplicados a componente login

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -19,9 +19,10 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <form onSubmit={(e) => handleSubmit(e)}>
-        <h1>
+    <div className='flex justify-center w-full h-screen items-center bg-white'>
+      <form onSubmit={(e) => handleSubmit(e)} 
+            className='flex flex-col items-center justify-center bg-purple-300 w-[600px] h-screen md:h-[450px] rounded-lg'>
+        <h1 className='mb-10 font-extrabold text-3xl text-white'>
           Sign Up!
         </h1>
         <input
@@ -29,19 +30,24 @@ const Login = () => {
           placeholder='user name'
           value={userName}
           onChange={(e) => setUserName(e.target.value)}
+          className='w-2/3 mb-4 text-gray-600 rounded bg-gray-100 h-[30px] pl-2' 
         />
         <input
           type='text'
           placeholder='password'
           value={userPwd}
           onChange={(e) => setUserPwd(e.target.value)}
+          className='w-2/3 mb-4 text-gray-600 rounded bg-gray-100 h-[30px] pl-2'
         />
-        <button 
-          type='submit'
-          onClick={() => {
-            console.log(userName, userPwd);
-          }}
-        >Submit</button>
+        
+        <div className='pt-4'>
+          <button 
+            type='submit'
+            className='bg-orange-400 text-white py-2 px-6 rounded hover:bg-orange-500 font-semibold
+            duration-500'
+          >Submit</button>
+        </div>
+
       </form>
     </div>
   )
