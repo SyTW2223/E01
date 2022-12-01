c61ba66c7b900e818960d5424c08056d585738ce
juanmdom <juanmdom@gmail.com>
2022-12-01 16:44:19 +0000

    logout btn

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import { logout } from '../features/userSlice';
 
 const Home = () => {
+  const dispatch = useDispatch();
+
+  const handleClick = (e)  => {
+    e.preventDefault();
+
+    dispatch(logout());
+  };
+  
   return (
-    <h1>Home</h1>
+    <div className='flex justify-center flex-col items-center'>
+      <h1>Home</h1>
+      <button 
+        onClick={(e) => handleClick(e)}
+        className='bg-orange-400 text-white py-2 px-6 rounded hover:bg-orange-500 font-semibold
+        duration-500'
+      > Logout </button>
+    </div>
+    
   )
 }
 
